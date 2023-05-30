a9dc0e84a86a34d62a12b8463b5b84328e55c2ee
Nestor162 <nestorcicardini@gmail.com>
2023-05-30 15:55:00 +0200

    Completata Classe Order

--- src/main/java/com/nestorcicardini/D2/entities/Order.java
+++ src/main/java/com/nestorcicardini/D2/entities/Order.java
@@ -1,5 +1,7 @@
 package com.nestorcicardini.D2.entities;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,5 +12,27 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class Order {
+	private List<Product> products;
+	private int orderNumber;
+	private OrderStatus orderStatus;
+	private int Occupancy;
+	private String orderTime;
+	private double totalAmount;
+
+	enum OrderStatus {
+		READY, IN_PROGRESS, SERVED
+	}
+
+	public Order(List<Product> products, int orderNumber,
+			OrderStatus orderStatus, int occupancy, String orderTime,
+			double totalAmount) {
+		super();
+		this.products = products;
+		this.orderNumber = orderNumber;
+		this.orderStatus = orderStatus;
+		Occupancy = occupancy;
+		this.orderTime = orderTime;
+		this.totalAmount = totalAmount;
+	}
 
 }
