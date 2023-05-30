c861e186908c184b8b658286167c2fcbb6f54ee5
Nestor162 <nestorcicardini@gmail.com>
2023-05-30 15:54:42 +0200

    Aggiornato runner per Menu

--- src/main/java/com/nestorcicardini/D2/MenuRunner.java
+++ src/main/java/com/nestorcicardini/D2/MenuRunner.java
@@ -1,8 +1,11 @@
 package com.nestorcicardini.D2;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.nestorcicardini.D2.entities.Topping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,8 +20,44 @@ public class MenuRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		log.info("Stampando ordine...");
+		System.out.println();
+		log.info("Benvenuti su Godfather's Pizza!" + System.lineSeparator());
+		log.info("::::::::::::::::::::::: Menu ::::::::::::::::::::::::::");
+		log.info("--- Pizzas ---");
+		configWithAnnotation();
 
+	}
+
+	public static void configWithAnnotation() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				D2Application.class);
+
+		log.info(context.getBean("pizzaMargherita").toString());
+		log.info(context.getBean("hawaiianPizza").toString());
+		log.info(context.getBean("salamiPizza").toString());
+		log.info(context.getBean("familyPizza").toString());
+
+		System.out.println();
+		log.info("--- Toppings ---");
+		log.info(context.getBean("cheese", Topping.class).showCompleteInfo());
+		log.info(context.getBean("ham", Topping.class).showCompleteInfo());
+		log.info(context.getBean("onions", Topping.class).showCompleteInfo());
+		log.info(
+				context.getBean("pineapple", Topping.class).showCompleteInfo());
+		log.info(context.getBean("salami", Topping.class).showCompleteInfo());
+
+		System.out.println();
+		log.info("--- Drinks ---");
+		log.info(context.getBean("lemonade").toString());
+		log.info(context.getBean("water").toString());
+		log.info(context.getBean("wine").toString());
+
+		System.out.println();
+		log.info("--- Franchise ---");
+		log.info(context.getBean("shirt").toString());
+		log.info(context.getBean("mug").toString());
+
+		context.close();
 	}
 
 }
