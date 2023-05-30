03db003c33824d769a5829a866ef5a233d124d7c
Nestor162 <nestorcicardini@gmail.com>
2023-05-30 15:05:16 +0200

    Creata classe Order

---
+++ src/main/java/com/nestorcicardini/D2/entities/Order.java
@@ -0,0 +1,14 @@
+package com.nestorcicardini.D2.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Order {
+
+}
---
+++ src/main/java/com/nestorcicardini/D2/entities/Table.java
@@ -0,0 +1,28 @@
+package com.nestorcicardini.D2.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Table {
+	private int tableNumber;
+	private int maxOccupancy;
+	private Status status;
+
+	enum Status {
+		OCCUPIED, AVAILABLE
+	}
+
+	public Table(int tableNumber, int maxOccupancy, Status status) {
+		super();
+		this.tableNumber = tableNumber;
+		this.maxOccupancy = maxOccupancy;
+		this.status = status;
+	}
+
+}
